1b8cb7dd456a8442dcfa54aa7d92fe0b6363da3c
successPhil <pbasti1990@gmail.com>
2024-04-21 20:10:07 -0400

    first commit

---
+++ run-compose-prod.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# The Dockerhub account where the images are stored
+export DOCKERHUB_UNAME=successphil
+export SECRET_KEY=abc123
+export DEBUG=True
+export POSTGRES_DB=pokemon_jwt_db
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+export NEW_VERSION=$NEW_VERSION
+
+
+docker-compose -f docker-compose.prod.yml build --no-cache
+docker-compose -f docker-compose.prod.yml up -d
+
+# make sure the postgres container is ready, then run migrations
+sleep 10 
+docker exec djoser-tutorial-api-1 python /src/manage.py makemigrations 
+docker exec djoser-tutorial-api-1 python /src/manage.py migrate
